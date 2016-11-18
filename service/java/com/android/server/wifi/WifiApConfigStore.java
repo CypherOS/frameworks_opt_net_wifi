280de160675f87e47e7d03179502e7b607e68ab5
bnarasimha <bnarasimha@cyngn.com>
2016-11-18 10:29:38 -0800

    Tethering: Turn off wifi hotspot after inactivity(2/3)
    Turn off the Wi-Fi hotspot after a specified time of inactivity.
    The hotspot is considered to be inactive when no clients are
    connected to it.
    
    Change-Id: I26ad7027a57df8109c87cc08b6887b5af108bbaf

--- service/java/com/android/server/wifi/WifiApConfigStore.java
+++ service/java/com/android/server/wifi/WifiApConfigStore.java
@@ -146,6 +146,10 @@ public class WifiApConfigStore {
             if (authType != KeyMgmt.NONE) {
                 config.preSharedKey = in.readUTF();
             }
+			// read in wifiApInactivityTimeout if bytes are available from in
+            if (in.available() != 0) {
+                config.wifiApInactivityTimeout = in.readLong();
+            }
         } catch (IOException e) {
             Log.e(TAG, "Error reading hotspot configuration " + e);
             config = null;
@@ -177,6 +181,7 @@ public class WifiApConfigStore {
             if (authType != KeyMgmt.NONE) {
                 out.writeUTF(config.preSharedKey);
             }
+			out.writeLong(config.wifiApInactivityTimeout);
         } catch (IOException e) {
             Log.e(TAG, "Error writing hotspot configuration" + e);
         }
@@ -196,6 +201,7 @@ public class WifiApConfigStore {
         String randomUUID = UUID.randomUUID().toString();
         //first 12 chars from xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
         config.preSharedKey = randomUUID.substring(0, 8) + randomUUID.substring(9, 13);
+		config.wifiApInactivityTimeout = 0;
         return config;
     }
 }
